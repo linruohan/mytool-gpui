274bba81f9871f5349dd7cd5f12be28a2e5447c2
linruohan <mjt1220@126.com>
2025-04-04 22:56:01 +0800

    add app icon

--- crates/mytool/src/main.rs
+++ crates/mytool/src/main.rs
@@ -1,3 +1,8 @@
+// Turns off console window on Windows, but not when building with dev profile.
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 #![allow(dead_code)]
 use gpui::*;
 
