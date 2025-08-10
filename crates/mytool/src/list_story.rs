404ea06f7201beb3fda0b3eb3d579e9567071c2c
linruohan <mjt1220@126.com>
2025-08-10 11:18:22 +0800

    add

--- crates/mytool/src/list_story.rs
+++ crates/mytool/src/list_story.rs
@@ -1,9 +1,10 @@
-use std::rc::Rc;
+use std::{rc::Rc, time::Duration};
 
+use fake::Fake;
 use gpui::{
     actions, div, prelude::FluentBuilder as _, px, App, AppContext, Context, Edges, ElementId,
     Entity, FocusHandle, Focusable, InteractiveElement, IntoElement, ParentElement, Render,
-    RenderOnce, ScrollStrategy, SharedString, Styled, Subscription, Task, Window,
+    RenderOnce, ScrollStrategy, SharedString, Styled, Subscription, Task, Timer, Window,
 };
 
 use gpui_component::{
@@ -12,31 +13,31 @@ use gpui_component::{
     h_flex,
     label::Label,
     list::{List, ListDelegate, ListEvent, ListItem},
-    v_flex, ActiveTheme, IndexPath, Selectable, Sizable,
+    v_flex, ActiveTheme, Icon, IconName, IndexPath, Selectable, Sizable,
 };
 use todos::entity::ProjectModel;
 
 use crate::{get_projects, DBState};
 
-actions!(list_story, [SelectedProject]);
+actions!(list_story, [SelectedMenu]);
 
 #[derive(IntoElement)]
-struct ProjectListItem {
+struct MenuListItem {
     base: ListItem,
     ix: IndexPath,
-    project: Rc<ProjectModel>,
+    menu: Rc<ProjectModel>,
     selected: bool,
 }
 
-impl ProjectListItem {
+impl MenuListItem {
     pub fn new(
         id: impl Into<ElementId>,
-        project: Rc<ProjectModel>,
+        menu: Rc<ProjectModel>,
         ix: IndexPath,
         selected: bool,
     ) -> Self {
-        ProjectListItem {
-            project,
+        MenuListItem {
+            menu,
             ix,
             base: ListItem::new(id),
             selected,
@@ -44,7 +45,7 @@ impl ProjectListItem {
     }
 }
 
-impl Selectable for ProjectListItem {
+impl Selectable for MenuListItem {
     fn selected(mut self, selected: bool) -> Self {
         self.selected = selected;
         self
@@ -55,19 +56,28 @@ impl Selectable for ProjectListItem {
     }
 }
 
-impl RenderOnce for ProjectListItem {
+impl RenderOnce for MenuListItem {
     fn render(self, _: &mut Window, cx: &mut App) -> impl IntoElement {
         let text_color = if self.selected {
             cx.theme().accent_foreground
         } else {
             cx.theme().foreground
         };
+        let bg_color = if self.selected {
+            cx.theme().list_active
+        } else if self.ix.row % 2 == 0 {
+            cx.theme().list
+        } else {
+            cx.theme().list_even
+        };
 
         self.base
             .px_2()
             .py_1()
             .overflow_x_hidden()
+            .bg(bg_color)
             .border_1()
+            .border_color(bg_color)
             .when(self.selected, |this| {
                 this.border_color(cx.theme().list_active_border)
             })
@@ -78,23 +88,42 @@ impl RenderOnce for ProjectListItem {
                     .justify_between()
                     .gap_2()
                     .text_color(text_color)
-                    .child(Label::new(self.project.id.clone()).whitespace_nowrap())
-                    .child(Label::new(self.project.name.clone()).whitespace_nowrap()),
+                    .child(
+                        h_flex()
+                            .gap_2()
+                            .items_center()
+                            .justify_end()
+                            .child(
+                                div()
+                                    .w(px(65.))
+                                    .text_color(text_color)
+                                    .child(self.menu.id.clone()),
+                            )
+                            .child(
+                                h_flex().w(px(65.)).justify_end().child(
+                                    div()
+                                        .rounded(cx.theme().radius)
+                                        .whitespace_nowrap()
+                                        .text_size(px(12.))
+                                        .px_1()
+                                        .child(self.menu.name.clone()),
+                                ),
+                            ),
+                    ),
             )
     }
 }
 
-struct ProjectListDelegate {
+struct MenuListDelegate {
     industries: Vec<SharedString>,
     _menus: Vec<Rc<ProjectModel>>,
     matched_menus: Vec<Vec<Rc<ProjectModel>>>,
     selected_index: Option<IndexPath>,
     confirmed_index: Option<IndexPath>,
     query: SharedString,
-    loading: bool,
 }
 
-impl ProjectListDelegate {
+impl MenuListDelegate {
     fn new() -> Self {
         Self {
             industries: vec![],
@@ -102,15 +131,7 @@ impl ProjectListDelegate {
             matched_menus: vec![],
             selected_index: None,
             confirmed_index: None,
-            query: SharedString::default(),
-            loading: false,
-        }
-    }
-    fn update_menus(&mut self, menus: Vec<Rc<ProjectModel>>) {
-        self._menus = menus;
-        self.matched_menus = vec![self._menus.clone()];
-        if !self.matched_menus.is_empty() && self.selected_index.is_none() {
-            self.selected_index = Some(IndexPath::new(1));
+            query: "".into(),
         }
     }
     fn prepare(&mut self, query: impl Into<SharedString>) {
@@ -118,18 +139,25 @@ impl ProjectListDelegate {
         let companies: Vec<Rc<ProjectModel>> = self
             ._menus
             .iter()
-            .filter(|company| {
-                company
-                    .name
+            .filter(|menu| {
+                menu.name
                     .to_lowercase()
                     .contains(&self.query.to_lowercase())
             })
             .cloned()
             .collect();
-        self.matched_menus = vec![companies];
+        for menu in companies.into_iter() {
+            self.matched_menus.push(vec![menu]);
+        }
     }
-
-    fn selected_company(&self) -> Option<Rc<ProjectModel>> {
+    fn update_menus(&mut self, menus: Vec<Rc<ProjectModel>>) {
+        self._menus = menus;
+        self.matched_menus = vec![self._menus.clone()];
+        if !self.matched_menus.is_empty() && self.selected_index.is_none() {
+            self.selected_index = Some(IndexPath::new(0));
+        }
+    }
+    fn selected_menu(&self) -> Option<Rc<ProjectModel>> {
         let Some(ix) = self.selected_index else {
             return None;
         };
@@ -141,11 +169,15 @@ impl ProjectListDelegate {
     }
 }
 
-impl ListDelegate for ProjectListDelegate {
-    type Item = ProjectListItem;
+impl ListDelegate for MenuListDelegate {
+    type Item = MenuListItem;
 
     fn sections_count(&self, _: &App) -> usize {
         self.industries.len()
+    }
+
+    fn items_count(&self, section: usize, _: &App) -> usize {
+        self.matched_menus[section].len()
     }
 
     fn perform_search(
@@ -158,21 +190,9 @@ impl ListDelegate for ProjectListDelegate {
         Task::ready(())
     }
 
-    fn items_count(&self, section: usize, _: &App) -> usize {
-        self.matched_menus[section].len()
-    }
-
-    fn render_item(
-        &self,
-        ix: IndexPath,
-        _: &mut Window,
-        _: &mut Context<List<Self>>,
-    ) -> Option<Self::Item> {
-        let selected = Some(ix) == self.selected_index || Some(ix) == self.confirmed_index;
-        if let Some(company) = self.matched_menus[ix.section].get(ix.row) {
-            return Some(ProjectListItem::new(ix, company.clone(), ix, selected));
-        }
-        None
+    fn confirm(&mut self, secondary: bool, window: &mut Window, cx: &mut Context<List<Self>>) {
+        println!("Confirmed with secondary: {}", secondary);
+        window.dispatch_action(Box::new(SelectedMenu), cx);
     }
 
     fn set_selected_index(
@@ -185,15 +205,24 @@ impl ListDelegate for ProjectListDelegate {
         cx.notify();
     }
 
-    fn confirm(&mut self, secondary: bool, window: &mut Window, cx: &mut Context<List<Self>>) {
-        println!("Confirmed with secondary: {}", secondary);
-        window.dispatch_action(Box::new(SelectedProject), cx);
+    fn render_item(
+        &self,
+        ix: IndexPath,
+        _: &mut Window,
+        _: &mut Context<List<Self>>,
+    ) -> Option<Self::Item> {
+        let selected = Some(ix) == self.selected_index || Some(ix) == self.confirmed_index;
+        if let Some(menu) = self.matched_menus[ix.section].get(ix.row) {
+            return Some(MenuListItem::new(ix, menu.clone(), ix, selected));
+        }
+
+        None
     }
 }
 
 pub struct ListStory {
     focus_handle: FocusHandle,
-    menu_list: Entity<List<ProjectListDelegate>>,
+    menu_list: Entity<List<MenuListDelegate>>,
     selected_menu: Option<Rc<ProjectModel>>,
     _subscriptions: Vec<Subscription>,
 }
@@ -218,25 +247,23 @@ impl ListStory {
     }
 
     fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
-        let company_list = cx.new(|cx| {
-            List::new(ProjectListDelegate::new(), window, cx).paddings(Edges::all(px(8.)))
-        });
+        let menu_list = cx
+            .new(|cx| List::new(MenuListDelegate::new(), window, cx).paddings(Edges::all(px(8.))));
 
-        let _subscriptions =
-            vec![
-                cx.subscribe(&company_list, |_, _, ev: &ListEvent, _| match ev {
-                    ListEvent::Select(ix) => {
-                        println!("List Selected: {:?}", ix);
-                    }
-                    ListEvent::Confirm(ix) => {
-                        println!("List Confirmed: {:?}", ix);
-                    }
-                    ListEvent::Cancel => {
-                        println!("List Cancelled");
-                    }
-                }),
-            ];
-        let company_list_clone = company_list.clone();
+        let _subscriptions = vec![
+            cx.subscribe(&menu_list, |_, _, ev: &ListEvent, _| match ev {
+                ListEvent::Select(ix) => {
+                    println!("List Selected: {:?}", ix);
+                }
+                ListEvent::Confirm(ix) => {
+                    println!("List Confirmed: {:?}", ix);
+                }
+                ListEvent::Cancel => {
+                    println!("List Cancelled");
+                }
+            }),
+        ];
+        let company_list_clone = menu_list.clone();
         let db = cx.global::<DBState>().conn.clone();
         cx.spawn(async move |_view, cx| {
             let db = db.lock().await;
@@ -252,16 +279,16 @@ impl ListStory {
         .detach();
         Self {
             focus_handle: cx.focus_handle(),
-            menu_list: company_list,
+            menu_list,
             selected_menu: None,
             _subscriptions,
         }
     }
 
-    fn selected_company(&mut self, _: &SelectedProject, _: &mut Window, cx: &mut Context<Self>) {
+    fn selected_menu(&mut self, _: &SelectedMenu, _: &mut Window, cx: &mut Context<Self>) {
         let picker = self.menu_list.read(cx);
-        if let Some(company) = picker.delegate().selected_company() {
-            self.selected_menu = Some(company);
+        if let Some(menu) = picker.delegate().selected_menu() {
+            self.selected_menu = Some(menu);
         }
     }
 }
@@ -276,7 +303,7 @@ impl Render for ListStory {
     fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         v_flex()
             .track_focus(&self.focus_handle)
-            .on_action(cx.listener(Self::selected_company))
+            .on_action(cx.listener(Self::selected_menu))
             .size_full()
             .gap_4()
             .child(
@@ -354,17 +381,6 @@ impl Render for ListStory {
                                             cx,
                                         );
                                     }
-                                })
-                            })),
-                    )
-                    .child(
-                        Checkbox::new("loading")
-                            .label("Loading")
-                            .checked(self.menu_list.read(cx).delegate().loading)
-                            .on_click(cx.listener(|this, check: &bool, _, cx| {
-                                this.menu_list.update(cx, |this, cx| {
-                                    this.delegate_mut().loading = *check;
-                                    cx.notify();
                                 })
                             })),
                     ),
